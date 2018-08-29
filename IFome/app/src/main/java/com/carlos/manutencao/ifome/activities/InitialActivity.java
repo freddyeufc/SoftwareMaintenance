bc0d71c39caf8125ce36a66e8611001114bbb2af
Carlos Victor <victordantasdantas97@gmail.com>
2018-08-28 23:54:44 -0300

    I've had started the First Frame of IFome App

--- IFome/app/src/main/java/com/carlos/manutencao/ifome/activities/InitialActivity.java
+++ IFome/app/src/main/java/com/carlos/manutencao/ifome/activities/InitialActivity.java
@@ -1,15 +1,42 @@
 package com.carlos.manutencao.ifome.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.carlos.manutencao.ifome.R;
+import com.carlos.manutencao.ifome.adapters.AdapterViewPagerInitial;
+import com.carlos.manutencao.ifome.fragments.InitialFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InitialActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        initViewPager();
+
+
+    }
+
+    private void initViewPager(){
+        viewPager = findViewById(R.id.viewPagerInitialId);
+
+        List<Fragment> listFragments = new ArrayList<>();
+
+        listFragments.add(InitialFragment.newInstance(R.drawable.ic_android_black_24dp, "Teste 01"));
+        listFragments.add(InitialFragment.newInstance(R.drawable.ic_android_black_24dp, "Teste 02"));
+
+        AdapterViewPagerInitial adapter = new AdapterViewPagerInitial(getSupportFragmentManager(), listFragments);
+
+        viewPager.setOffscreenPageLimit(listFragments.size());
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ IFome/app/src/main/java/com/carlos/manutencao/ifome/adapters/AdapterViewPagerInitial.java
@@ -0,0 +1,27 @@
+package com.carlos.manutencao.ifome.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class AdapterViewPagerInitial extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public AdapterViewPagerInitial(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return this.fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
