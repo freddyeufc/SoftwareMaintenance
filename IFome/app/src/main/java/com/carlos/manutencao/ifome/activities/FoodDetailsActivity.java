6fc07ae0e0a17328f6f9a986515486132b572d64
almeidavinicius <vinias@alu.ufc.br>
2018-09-13 23:30:17 -0300

    Atualizando .git_ignore

---
+++ IFome/app/src/main/java/com/carlos/manutencao/ifome/activities/FoodDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.carlos.manutencao.ifome.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.carlos.manutencao.ifome.R;
+
+
+public class FoodDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_details);
+    }
+}
